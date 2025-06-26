8ac87a347b40287bd44681a108489bad64fe45d4
Mayank kumar <mayank.kumarace@gmail.com>
2025-06-26 17:55:39 +0530

    cechecj

--- component/common/SEO.js
+++ component/common/SEO.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const SEO = ({ title , description }) => {
+  const router = useRouter()
   return (
     <>
          <Head>
         <title>{title}</title>
+        <meta name="canonical" content={`https://www.thevoyagetours.com${router.asPath}`} />
+
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
